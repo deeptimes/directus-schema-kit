692777fefc9c50883d6da200776d119d3cd129c7
Michael <leosin@qq.com>
2026-06-19 17:09:06 +0800

    1

--- playground/dsk/resources/folders.ts
+++ playground/dsk/resources/folders.ts
@@ -1,16 +1,2 @@
-import { ref, resource } from '@deeptimes/directus-schema-kit'
-
-export default [
-  resource('folders', {
-    key: 'course-assets',
-    data: { name: '课程资源', parent: null },
-  }),
-  resource('folders', {
-    key: 'course-covers',
-    data: { name: '课程封面', parent: ref('folders.course-assets') },
-  }),
-  resource('folders', {
-    key: 'lesson-attachments',
-    data: { name: '课时附件', parent: ref('folders.course-assets') },
-  }),
-]
+/** 安全的空 Resource 定义。 */
+export default []
--- playground/dsk/resources/roles.ts
+++ playground/dsk/resources/roles.ts
@@ -1,16 +1,2 @@
-import { resource } from '@deeptimes/directus-schema-kit'
-
-export default [
-  resource('roles', {
-    key: 'student',
-    data: { name: '学员', icon: 'school', description: '购买课程并完成学习的用户。', parent: null },
-  }),
-  resource('roles', {
-    key: 'instructor',
-    data: { name: '讲师', icon: 'co_present', description: '维护本人课程及课程内容。', parent: null },
-  }),
-  resource('roles', {
-    key: 'operator',
-    data: { name: '课程运营', icon: 'support_agent', description: '管理课程、交易、报名和评价。', parent: null },
-  }),
-]
+/** 安全的空 Resource 定义。 */
+export default []
